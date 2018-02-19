d6d7789d36e2f8f88da658669dd6ad65069eb661
TylerWasniowski <TylerWasniowski@gmail.com>
2018-02-18 21:48:23 -0800

    Finished making database module

--- src/db.rs
+++ src/db.rs
@@ -1,34 +1,72 @@
-use std::clone::Clone;
+use std::ops::Fn;
+use std::ops::FnMut;
 
 #[derive(Debug)]
 pub struct Database<T> {
     data: Vec<T>,
 }
 
-impl <T> Database<T> {
+impl<T> Database<T> {
     pub fn add(&mut self, data: T) {
         self.data.push(data);
     }
 
-    pub fn read_all(&self) -> Vec<T> where T: Clone {
-        self.data.to_vec()
+    pub fn read_all(&self) -> &Vec<T> {
+        &self.data
     }
 
-//    pub fn select_first(&self, predicate: &Fn(T) -> bool) -> Option<T> {
-//        self.data.into_iter().find(predicate)
-//    }
-//
-//    pub fn select_all(&self, predicate: &Fn(T) -> bool) -> Option<Vec<T>> {
-//        self.data.into_iter().filter(predicate).collect::<Vec<T>>()
-//    }
-//
-//    pub fn remove_first(&self, predicate: &Fn(T) -> bool) {
-//        self.data.remove_item(self.select_first(predicate));
-//    }
-//
-//    pub fn remove_all(&self, predicate: &Fn(T) -> bool) {
-//        self.data.retain(|&data: T| !predicate(data));
-//    }
+    pub fn get(&self, i: usize) -> Option<&T> {
+        self.data.get(i)
+    }
+
+    pub fn find_first<F: Fn(&&T) -> bool>(&self, predicate: F) -> Option<&T> {
+        self.data.iter()
+            .find(predicate)
+    }
+
+    pub fn find_all<F: Fn(&&T) -> bool>(&self, predicate: F) -> Vec<&T> {
+        self.data.iter()
+            .filter(predicate)
+            .collect::<Vec<&T>>()
+    }
+
+    pub fn replace(&mut self, i: usize, new_value: T) {
+        if let Some(value) = self.data.get_mut(i) {
+            *value = new_value;
+        }
+    }
+
+    pub fn replace_first<F: FnMut(&&mut T) -> bool>(&mut self, predicate: F, new_value: T) {
+        if let Some(value) = self.data.iter_mut()
+            .find(predicate)
+        {
+            *value = new_value;
+        }
+    }
+
+    pub fn replace_all<F>(&mut self, predicate: F, new_value: T)
+        where F: Fn(&T) -> bool,
+              T: Clone,
+    {
+        self.data.iter_mut()
+            .for_each(|value| {
+                if predicate(value) {
+                    *value = new_value.clone();
+                }
+            });
+    }
+
+    pub fn remove_first<F: Fn(&T) -> bool>(&mut self, predicate: F) {
+        if let Some(i) = self.data.iter()
+            .position(predicate)
+        {
+            self.data.remove(i);
+        }
+    }
+
+    pub fn remove_all<F: Fn(&T) -> bool>(&mut self, predicate: F) {
+        self.data.retain(|data| !predicate(data));
+    }
 
     pub fn new() -> Database<T> {
         Database {
--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,45 @@ fn main() {
     let mut database: Database<String> = Database::new();
     database.add(String::from("Hello world!"));
     database.add(String::from("Hi"));
+    database.add("Hey".to_string());
+    database.add("Hello".to_string());
+    database.add(String::from("Hola mundo!"));
+    database.add("Yo".to_string());
+
+    match database.find_first(|data| data.len() < 5) {
+        Some(s) => println!("Hello {}", s),
+        None => (),
+    }
+
+    println!("Database: {:?}", database.read_all());
+    println!("Find first string less than 5: {:?}",
+             database.find_first(|data| data.len() < 5));
+    println!("Find all strings less than 5: {:?}",
+             database.find_all(|data| data.len() < 5));
+    println!();
+    println!("Removing first string with length less than 5.");
+    database.remove_first(|data| data.len() < 5);
+    println!("Database: {:?}", database.read_all());
+    println!();
+    println!("Removing first string with length less than 5.");
+    database.remove_first(|data| data.len() < 5);
+    println!("Database: {:?}", database.read_all());
+    println!();
+    println!("Replacing second value with world.");
+    database.replace(1, "World".to_string());
+    println!("Database: {:?}", database.read_all());
+    println!();
+    println!("Replacing first string with length equal to 5 with 'Hello'.");
+    database.replace_first(|data| data.len() == 5,
+                           "Hello".to_string());
+    println!("Database: {:?}", database.read_all());
+    println!();
+    println!("Replacing all strings with length less than or equal to 5 with 'Goodbye'.");
+    database.replace_all(|data| data.len() <= 5,
+                           "Goodbye".to_string());
+    println!("Database: {:?}", database.read_all());
+    println!();
+    println!("Removing all strings with length less than 8.");
+    database.remove_all(|value| value.len() < 8);
     println!("Database: {:?}", database.read_all());
 }

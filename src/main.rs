21feb3e38a240fcbfafefd66f38cd2baf5b46727
TylerWasniowski <TylerWasniowski@gmail.com>
2018-02-21 22:38:35 -0800

    Started moving tests

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ extern crate regex;
 pub mod db;
 pub mod to_do_list;
 
-use db::Database;
 use to_do_list::ToDoList;
 
 use regex::Regex;
@@ -11,23 +10,6 @@ use regex::Regex;
 fn main() {
     // Database test
     // TODO: Move to an actual tester
-//    let mut database: Database<String> = Database::new();
-//    database.add(String::from("Hello world!"));
-//    database.add(String::from("Hi"));
-//    database.add("Hey".to_string());
-//    database.add("Hello".to_string());
-//    database.add(String::from("Hola mundo!"));
-//    database.add("Yo".to_string());
-//
-//    println!("Database: {:?}", database.read_all());
-//    println!("Positions of strings with length less than 5: {:?}",
-//             database.position_all(|value| value.len() < 5));
-//    println!();
-//    println!("Find first string less than 5: {:?}",
-//             database.find_first(|data| data.len() < 5));
-//    println!("Find all strings less than 5: {:?}",
-//             database.find_all(|data| data.len() < 5));
-//    println!();
 //    println!("Removing first string with length less than 5.");
 //    database.remove_first(|data| data.len() < 5);
 //    println!("Database: {:?}", database.read_all());
@@ -72,4 +54,65 @@ fn main() {
     to_do_list.mark(0, true);
     println!("ToDoList:");
     println!("{}", to_do_list);
+}
+
+#[cfg(test)]
+mod database_tests {
+    use db::Database;
+
+    #[test]
+    fn position_first() {
+        let empty_database: Database<String> = Database::new();
+        let database = setup_database();
+        assert_eq!(empty_database.position_first(|data| data.len() < 5),
+                   None);
+        assert_eq!(database.position_first(|data| data.len() < 5),
+                   Some(1));
+    }
+
+    #[test]
+    fn position_all() {
+        let empty_database: Database<String> = Database::new();
+        let database = setup_database();
+        assert_eq!(empty_database.position_all(|data| data.len() < 5),
+                   Vec::new());
+        assert_eq!(database.position_all(|value| value.len() < 5),
+                   vec![1, 2, 5]);
+    }
+
+    #[test]
+    fn find_first() {
+        let empty_database: Database<String> = Database::new();
+        let database = setup_database();
+        assert_eq!(empty_database.find_first(|data| data.len() < 5),
+                   None);
+        assert_eq!(database.find_first(|data| data.len() < 5),
+                   Some(&"Hi".to_string()));
+    }
+
+    #[test]
+    fn find_all() {
+        let empty_database: Database<String> = Database::new();
+        let database = setup_database();
+        assert_eq!(empty_database.find_all(|value| value.len() < 5),
+            Vec::new() as Vec<&String>);
+        assert_eq!(database.find_all(|value| value.len() > 5),
+                   vec![&"Hello world!".to_string(), &"Hola mundo!".to_string()]);
+        assert_eq!(database.find_all(|value| value == &&"Hello".to_string()),
+                   vec![&"Hello".to_string()]);
+        assert_eq!(database.find_all(|value| value.len() < 5),
+                   vec![&"Hi".to_string(), &"Hey".to_string(), &"Yo".to_string()]);
+    }
+
+    fn setup_database() -> Database<String> {
+        let mut database: Database<String> = Database::new();
+        database.add("Hello world!".to_string());
+        database.add("Hi".to_string());
+        database.add("Hey".to_string());
+        database.add("Hello".to_string());
+        database.add("Hola mundo!".to_string());
+        database.add("Yo".to_string());
+
+        database
+    }
 }
